1e37f8b4ab0cb27e351f1fc86f6ad58da3ff927b
Wencong Xie <61610971+CodeNoob-xie@users.noreply.github.com>
2024-01-30 18:45:34 -0500

    Update Dice.java

--- CS 342/2/Dice.java
+++ CS 342/2/Dice.java
@@ -1,1 +1,10 @@
-
+public class Dice{
+  Die[]dice;
+  private int diceCount;
+  private int diceSize;
+  
+  public Dice(int diceCOunt, int dicSize){
+    this.diceCount = diceCount;
+    this.diceSize = diceSize; 
+    for(int i=0;i<diceCOunt;i++){
+     dice[i] = new Die(diceSize)
